f7ef7677f6e41ef345d65fef72856605fe8899c8
李雅涵 <2970648009@qq.com>
2026-05-28 23:13:13 +0800

    添加互斥锁示例

--- 02-thread/thread_sync.c
+++ 02-thread/thread_sync.c
@@ -1,1 +1,24 @@
- 
+#include <stdio.h>
+#include <pthread.h>
+
+int counter = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* increment(void* arg) {
+    for (int i = 0; i < 10000; i++) {
+        pthread_mutex_lock(&mutex);
+        counter++;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    printf("counter = %d (期望 20000)\n", counter);
+    return 0;
+}
