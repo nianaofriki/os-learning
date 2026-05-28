c708043ef0577da1c6b3d3054c5370ebdf27f7e8
李雅涵 <2970648009@qq.com>
2026-05-28 22:56:44 +0800

    添加线程创建示例

--- 02-thread/producer_consumer.c
+++ 02-thread/producer_consumer.c
@@ -1,1 +1,25 @@
- 
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void* thread_func(void* arg) {
+    int* num = (int*)arg;
+    printf("子线程 %d: PID=%d, TID=%lu\n", *num, getpid(), pthread_self());
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+    int arg1 = 1, arg2 = 2;
+    
+    // 创建线程
+    pthread_create(&t1, NULL, thread_func, &arg1);
+    pthread_create(&t2, NULL, thread_func, &arg2);
+    
+    // 等待线程结束
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    
+    printf("主线程退出\n");
+    return 0;
+} 
