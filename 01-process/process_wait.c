6a8a5e97ed89082d04d67e079a325189bf60a6d1
李雅涵 <2970648009@qq.com>
2026-05-28 14:19:19 +0800

    添加进程等待示例 (wait)

--- 01-process/process_wait.c
+++ 01-process/process_wait.c
@@ -1,1 +1,29 @@
- 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h> 
+
+int main() {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
+        return 1;
+    }
+    else if (pid == 0) {
+        printf("子进程 (PID=%d) 开始工作...\n", getpid());
+        sleep(2);
+        printf("子进程 (PID=%d) 工作完成，退出。\n", getpid());
+        exit(0);
+    }
+    else {
+        printf("父进程 (PID=%d) 等待子进程 (PID=%d) 结束...\n", getpid(), pid);
+        int status;
+        wait(&status);
+        printf("父进程：子进程已退出，回收成功。\n");
+        if (WIFEXITED(status)) {
+            printf("子进程正常退出，退出码：%d\n", WEXITSTATUS(status));
+        }
+    }
+    
+    return 0;
+}
