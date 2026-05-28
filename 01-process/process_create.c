36537d89529f1f10d85c4d87e20b484f60179e78
李雅涵 <2970648009@qq.com>
2026-05-28 13:44:26 +0800

    完成进程创建示例（fork）

--- 01-process/process_create.c
+++ 01-process/process_create.c
@@ -1,1 +1,20 @@
- 
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(){
+    pid_t pid = fork();
+    if(pid<0){
+        perror("fork failed");
+        return 1;
+    }
+    else if (pid == 0) {
+        printf("子进程: PID = %d, 父进程 PID = %d\n", getpid(), getppid());
+    } 
+    else {
+        printf("父进程: PID = %d, 子进程 PID = %d\n", getpid(), pid);
+        wait(NULL);
+    }
+    return 0;
+}
